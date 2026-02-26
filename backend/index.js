f270eebf33678a341b019d2da384e38596acae04
AGNIK DASTIDAR <147322194+agnik04dastidar@users.noreply.github.com>
2026-02-26 16:03:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://real-time-code-editor-7n13.onrender.com",
   methods: ["GET", "POST"],
   credentials: true,
 }));
@@ -22,7 +22,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://real-time-code-editor-7n13.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
